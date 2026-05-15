aaf78eb1748d63e996c2d5f72e57020706691581
YahirAgustin <yahirsoto200606@gmail.com>
2026-05-14 20:51:38 -0700

    app prototype

---
+++ App/src/navigation/AppNavigator.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LoginScreen from '../screens/LoginScreen';
+import PermissionsScreen from '../screens/PermissionsScreen';
+import PermissionsDeniedScreen from '../screens/PermissionsDeniedScreen';
+import PhotoCaptureScreen from '../screens/PhotoCaptureScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Login"
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: '#000000' },
+      }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Permissions" component={PermissionsScreen} />
+      <Stack.Screen name="PermissionsDenied" component={PermissionsDeniedScreen} />
+      <Stack.Screen name="PhotoCapture" component={PhotoCaptureScreen} />
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AppNavigator;
